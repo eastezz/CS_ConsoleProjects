13fa5733e845fdd51ff20cddf4ef8d83a2de8145
eastezz <basanderov1@gmail.com>
2026-03-15 21:25:41 +0100

    Create hash set to prevent letters duplication/Add comments

--- Hangman_Game/Hangman_Game/Program.cs
+++ Hangman_Game/Hangman_Game/Program.cs
@@ -10,25 +10,36 @@ class HangmanGame
         int guesses = 6;
         char letter;
         bool isCorrect;
+
+        // Fills the list with a symbols to create an apropriate interface
         foreach (char i in secretWord)
         {
             hiddenLetters.Add('_');
         }
 
+        // Used to prevent letters duplicate
+        HashSet<char> usedLetters = new HashSet<char>(); 
+
         while (guesses > 0)
         {
             isCorrect = false;
             Console.WriteLine("Current progress:");
-            foreach (char i in hiddenLetters)
+            Console.WriteLine(string.Join(" ", hiddenLetters));
+            Console.WriteLine($"You have {guesses} wrong guesses left.\n Guess a leter: ");
+
+            letter = char.ToLower(Console.ReadLine()[0]);
+
+            // Prevent repeated guesses
+            if (usedLetters.Contains(letter))
             {
-                Console.Write(i + " ");
+                Console.WriteLine("You already guessed that letter!");
+                Console.ReadKey();
+                continue;
             }
-            Console.WriteLine($"\n You have {guesses} wrong guesses left.");
 
-            Console.Write("Guess a letter: ");
-            letter = Console.ReadLine()[0];
-            letter = char.ToLower(letter);
+            usedLetters.Add(letter);
 
+            // Matchs the letter (main logic)
             for (int i = 0; i < secretWord.Length; i++)
             {
                 if (letter.Equals(secretWord[i]))
@@ -40,12 +51,14 @@ class HangmanGame
 
             if (!isCorrect) guesses--;
 
+            // Win condition
             if (!hiddenLetters.Contains('_'))
             {
                 Console.WriteLine($"Congratulations! You've guessed the word: {secretWord}");
                 return;
             }
         }
+        // Lose condition
         Console.WriteLine($"Game over! The word was {secretWord}");
     }
 }

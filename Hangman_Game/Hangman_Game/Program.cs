17c0e74fc624a1ffd44ef24f20810091e56f3794
eastezz <basanderov1@gmail.com>
2026-03-15 20:03:27 +0100

    Update/Fixed output bug

--- Hangman_Game/Hangman_Game/Program.cs
+++ Hangman_Game/Hangman_Game/Program.cs
@@ -1,44 +1,57 @@
-﻿Console.WriteLine("Enter the secrete word:");
-string secret_word = Console.ReadLine();
-List<char> hidden_letters = new List<char>();
-int guesses = 6;
-char letter;
-bool isCorrect = false;
-for(int i = 0; i < secret_word.Length; i++)
+﻿using System;
+using System.Collections.Generic;
+class HangmanGame
 {
-    hidden_letters.Add('_');
-}
-
-while(true)
-{
-    Console.WriteLine("Current progress:");
-    foreach(char i in hidden_letters)
+    static void Main()
     {
-        Console.Write(i + " ");
-    }
-    Console.WriteLine($"\n You have {guesses} wrong guesses left.");
-
-    Console.Write("Guess a letter: ");
-    letter = (char)Console.Read();
-
-    for(int i = 0; i < secret_word.Length; i++)
-    {
-        if ((int)letter == secret_word[i])
+        Console.WriteLine("Enter the secret word:");
+        string secretWord = Console.ReadLine().ToLower();
+        List<char> hiddenLetters = new List<char>();
+        int guesses = 6;
+        char letter;
+        bool isCorrect;
+        for (int i = 0; i < secretWord.Length; i++)
         {
-            hidden_letters[i] = (char)secret_word[i];
-            isCorrect = true;
+            hiddenLetters.Add('_');
+        }
+
+        while (true)
+        {
+            isCorrect = false;
+            Console.WriteLine("Current progress:");
+            foreach (char i in hiddenLetters)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine($"\n You have {guesses} wrong guesses left.");
+
+            Console.Write("Guess a letter: ");
+            letter = Console.ReadLine()[0];
+            letter = char.ToLower(letter);
+
+            for (int i = 0; i < secretWord.Length; i++)
+            {
+                if (letter.Equals(secretWord[i]) && !hiddenLetters.Contains(letter))
+                {
+                    hiddenLetters[i] = secretWord[i];
+                    isCorrect = true;
+                }
+            }
+
+            if (!isCorrect) guesses--;
+
+            if (guesses == 0)
+            {
+                Console.WriteLine($"Game over! The word was {secretWord}");
+                break;
+            }
+
+            if (!hiddenLetters.Contains('_'))
+            {
+                Console.WriteLine($"Congratulations! You've guessed the word: {secretWord}");
+                break;
+            }
         }
     }
-
-    if (!isCorrect) guesses--;
-    isCorrect = false;
-
-    for(int i = 0; i < secret_word.Length; i++)
-    {
-        string final_world = string.Join("",hidden_letters);
-    }
-
-
-    
 }
 
